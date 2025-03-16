eb9db455181cc34ff0d22c6f8a7a299129d6d4cd
HaibaraAi-afk <gamarramadhani16@gmail.com>
2025-03-16 15:05:19 +0700

    add NewsController and News model with migrations and seeder

---
+++ database/migrations/2025_03_16_053314_create_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id(); // Primary key (big integer, auto increment)
+            $table->string('title'); // Judul berita
+            $table->text('content'); // Isi berita
+            $table->string('image')->nullable(); // Gambar berita (opsional)
+            $table->foreignId('author_id')->constrained('users')->onDelete('cascade'); // Relasi ke tabel users
+            $table->foreignId('approved_by')->nullable()->constrained('users')->onDelete('set null'); // Relasi ke tabel users
+            $table->timestamp('published_at')->nullable(); // Waktu publikasi
+            $table->enum('status', ['draft', 'pending', 'approved', 'rejected'])->default('draft');
+            $table->timestamps(); // Kolom created_at & updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
