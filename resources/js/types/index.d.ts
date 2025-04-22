332137c650095cf0490f13c20d034e7118e6e858
HaibaraAi-afk <gamarramadhani16@gmail.com>
2025-04-22 20:40:08 +0700

    refactor User interface to rename avatar property to image

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -34,7 +34,7 @@ export interface User {
     id: number;
     name: string;
     email: string;
-    avatar?: string;
+    image?: string;
     email_verified_at: string | null;
     created_at: string;
     updated_at: string;
