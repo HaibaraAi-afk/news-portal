81c47f952fa78a3cf16bfb306a1f20168b9868f4
HaibaraAi-afk <gamarramadhani16@gmail.com>
2025-04-29 14:26:11 +0700

    refactor app.tsx to enhance page resolution logging and support for JSX files

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -9,7 +9,16 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+    resolve: name => {
+        console.log("Resolving page:", name);
+        return resolvePageComponent(
+            `./pages/${name}.tsx`,
+            import.meta.glob('./pages/**/*.{jsx,tsx}')
+        );
+    },
+    
+    
+      
     setup({ el, App, props }) {
         const root = createRoot(el);
 
